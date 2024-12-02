8a3b8046616ff933675c579c6181ee682d568ae6
syr1ne <167830358+syr1ne@users.noreply.github.com>
2024-12-02 19:46:40 +0530

    all images in ./screenshots

--- ssweb.py
+++ ssweb.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 
+import os
 from sys import argv, stdin
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+help = """
+for single page ss:
+  echo "https://site.com" | ./ssweb.py
+for list of url ss:
+  cat urls.txt | ./ssweb.py
+  -- or --
+  ./ssweb.py {file path}
+"""
 
 def main():
   options = Options()
   options.add_argument("--headless")
   driver = webdriver.Chrome(options=options)
-
-  help = """
-  cat urls.txt | ./ssweb.py
-  -- or --
-  python3 ssweb.py {file path}
-  """
 
   if not stdin.isatty():
     urls = stdin                                     
@@ -30,10 +34,15 @@ def main():
       print(help)
       exit()
 
+  ss_dir = os.path.join(os.getcwd(), "screenshots")
+  os.makedirs(ss_dir, exist_ok=True)
+
   for url in urls:
-    imageName = url.replace('.', '_').replace(':', '_').replace('/', '').strip() + ".png"
+
+    image_name = url.replace('.', '_').replace(':', '_').replace('/', '').strip() + ".png"
     driver.get(url)
-    driver.save_screenshot(imageName)
+    image_path = os.path.join(ss_dir, image_name)
+    driver.save_screenshot(image_path)
   driver.quit()
 
 if __name__=="__main__":
